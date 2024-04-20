dcf6e5953a1bea88a136000d533c82180d21fba4
gian <michaelgiantiqui3@gmail.com>
2024-04-20 17:42:46 +0800

    new navbar

--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -5,27 +5,8 @@ import { TbPokeball } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <nav className="hidden h-14 px-5 border-b border-b-gray-800 md:flex md:justify-between">
-      <div className="flex">
-        <Link href={"/"} className="flex items-center gap-2">
-          <TbPokeball className="h-7 w-auto text-white" />
-          <p className="font-mono text-white text-2xl font-bold italic">
-            Michael Gian Tiqui
-          </p>
-        </Link>
-      </div>
-
-      <div className="flex justify-end gap-10">
-        {routesMapping.map((route, index) => (
-          <Link
-            href={route.path}
-            className="font-mono text-white text-lg my-auto"
-            key={index}
-          >
-            {route.name}
-          </Link>
-        ))}
-      </div>
+    <nav className="hidden h-14 pt-14 md:flex md:justify-center">
+      <div className="rounded-lg shadow-2xl bg-neutral-950 h-14 w-[500px] flex items-center"></div>
     </nav>
   );
 };
