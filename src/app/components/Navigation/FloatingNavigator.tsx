2494b5275aac9fb3a993cce3ed85eb53cee22b3e
gian <michaelgiantiqui3@gmail.com>
2024-04-20 22:14:55 +0800

    added new routes

--- src/app/components/Navigation/FloatingNavigator.tsx
+++ src/app/components/Navigation/FloatingNavigator.tsx
@@ -28,14 +28,14 @@ const FloatingNavigator = () => {
             initial={{ opacity: 0, scale: 0.001 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.001 }}
-            className="hidden md:block fixed top-7 right-7 z-50 cursor-pointer bg-neutral-950 rounded-lg p-1"
+            className="hidden md:block fixed top-7 right-7 z-50 cursor-pointer bg-neutral-950 rounded-full p-4"
             onClick={() => alert("hi")}
           >
             <CgMenu className="text-white h-8 w-auto" />
           </motion.div>
         )}
       </AnimatePresence>
-      <div className="block md:hidden fixed top-7 right-7 z-50 cursor-pointer bg-neutral-950 rounded-lg p-1">
+      <div className="block md:hidden fixed top-7 right-7 z-50 cursor-pointer bg-neutral-950 rounded-full p-4">
         <CgMenu className="text-whit h-8 w-auto" />
       </div>
     </>
--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -6,7 +6,17 @@ import { TbPokeball } from "react-icons/tb";
 const Navbar = () => {
   return (
     <nav className="hidden h-14 pt-14 md:flex md:justify-center">
-      <div className="rounded-lg shadow-2xl bg-neutral-950 h-14 w-[500px] flex items-center"></div>
+      <div className="rounded-lg shadow-2xl bg-neutral-950 h-14 flex items-center justify-between gap-10 px-20">
+        {routesMapping.map((route, index) => (
+          <Link
+            className="font-mono text-white text-xl"
+            href={route.path}
+            key={index}
+          >
+            {route.name}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 };
