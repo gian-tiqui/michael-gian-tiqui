462e749ffc99cc3b1942fcc95b0029ba3676c81c
gian <michaelgiantiqui3@gmail.com>
2024-04-21 13:54:20 +0800

    idk what I added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="px-10 pt-48 md:px-64 md:pt-72 md: pb-32">
+    <div className="px-10 pt-48 md:px-64 md:pt-72 md: pb-32 h-[1500px]">
       <div className="md:max-w-[950px] mx-auto">
         <p className="text-3xl cursor-default font-mono md:text-5xl text-justify text-white font-extrabold leading-relaxed">
           Hello there 👋, I am{" "}
