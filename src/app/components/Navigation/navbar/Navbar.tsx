aa3da596c1f3c8f2fa4c9857b41fa40c8e8e197a
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-21 10:24:25 +0800

    added navbar animation

--- src/app/components/Navigation/navbar/Navbar.tsx
+++ src/app/components/Navigation/navbar/Navbar.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { routesMapping } from "../../../utils/Routes";
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
+import { routesMapping } from "@/app/utils/Routes";
 
 const Navbar = () => {
+  const [scrolledDown, setScrolledDown] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrolledDown(scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="hidden h-14 pt-14 md:flex md:justify-center">
+    <motion.nav
+      initial={{ opacity: 1, y: "50%" }}
+      animate={{ opacity: scrolledDown ? 0 : 1, y: scrolledDown ? 0 : "50%" }}
+      exit={{ opacity: 0, y: 0 }}
+      transition={{ duration: 0.3 }}
+      className="hidden fixed top-0 left-0 w-full h-14 pt-14 md:flex md:justify-center"
+    >
       <div className="rounded-full bg-neutral-900 h-14 flex items-center justify-between gap-10 px-20">
         {routesMapping.map((route, index) => (
           <Link
@@ -16,7 +37,7 @@ const Navbar = () => {
           </Link>
         ))}
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-[1500px] px-10 pt-48 md:p-48">
+    <div className="h-[1500px] px-10 pt-48 md:p-64">
       <div className="md:max-w-[950px] mx-auto">
         <p className="text-3xl cursor-default font-mono md:text-6xl text-justify text-white font-extrabold leading-relaxed">
           Hello there 👋, I am{" "}
