e2d91d4bce33d8ab9aed582a22cbc91c3edb9fa7
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-21 10:09:45 +0800

    tried some tailwind animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div className="h-[1500px] px-10 pt-48 md:p-48">
       <div className="md:max-w-[950px] mx-auto">
-        <p className="text-3xl cursor-default font-mono md:text-6xl text-justify text-white font-extrabold leading-tight">
-          Hey 👋, I am <span className="text-blue-500">Michael Gian Tiqui</span>
-          . An Aspiring Software Engineer from the City of San Pedro, Laguna,
-          Philippines
+        <p className="text-3xl cursor-default font-mono md:text-6xl text-justify text-white font-extrabold leading-relaxed">
+          Hello there 👋, I am{" "}
+          <span className="text-blue-500">Michael Gian Tiqui</span>. An Aspiring
+          Software Engineer from the City of San Pedro, Laguna, Philippines
         </p>
       </div>
     </div>
