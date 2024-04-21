1cd5f8927bd4e05c656cb17e15780567f51479ae
gian <michaelgiantiqui3@gmail.com>
2024-04-21 21:44:10 +0800

    changing introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import React from "react";
+import Introduction from "./components/introduction/Introduction";
 
 const Home = () => {
   return (
     <div className="px-10 pt-48 md:px-64 md:pt-72 md: pb-32 h-[1500px]">
-      <div className="md:max-w-[950px] mx-auto">
-        <p className="text-3xl cursor-default font-mono md:text-5xl text-justify text-white font-extrabold leading-relaxed">
-          Hello there 👋, I am{" "}
-          <span className="text-blue-500">Michael Gian Tiqui</span>. An Aspiring
-          Software Engineer from the City of San Pedro, Laguna, Philippines
-        </p>
-      </div>
+      <Introduction />
     </div>
   );
 };
