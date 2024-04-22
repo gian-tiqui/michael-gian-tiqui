3fdcc8e0945f6b4d562e9d8f3b7e29001624caea
gian <michaelgiantiqui3@gmail.com>
2024-04-22 10:45:50 +0800

    idk what I did but next is lightmode toggler

--- src/app/components/tech-stacks/components/TechStackContainer.tsx
+++ src/app/components/tech-stacks/components/TechStackContainer.tsx
@@ -10,7 +10,6 @@ const TechStackContainer = ({
 }) => {
   return (
     <div className="rounded-xl bg-neutral-900 h-20 px-10 w-64 flex justify-center items-center gap-3">
-      {/* add md:w-auto pag trip mo */}
       {children}
       <p className="text-white font-mono text-2xl font-bold">{name}</p>
     </div>
