4158a32b958f134eef3f33a74616544e9e739372
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-23 20:13:58 +0800

    meow

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { routesMapping } from "@/app/utils/Routes";
 import { CgCopyright } from "react-icons/cg";
 
+// hehe
+
 interface IconsRouteInterface {
   icon: ReactNode;
   path: string;
