46f8a54c4c0b6590df5071898e525b4266715999
gian <michaelgiantiqui3@gmail.com>
2024-04-27 17:17:44 +0800

    added programming languages

--- src/app/projects/components/sections/Expertise.tsx
+++ src/app/projects/components/sections/Expertise.tsx
@@ -4,33 +4,45 @@ import { FaLaravel, FaNodeJs, FaReact } from "react-icons/fa";
 import {
   SiAndroidstudio,
   SiArduino,
+  SiCplusplus,
+  SiCsharp,
   SiDotnet,
   SiElectron,
   SiExpress,
   SiFastapi,
   SiJquery,
+  SiKotlin,
   SiPrisma,
   SiSpringboot,
+  SiSqlite,
   SiTailwindcss,
+  SiTypescript,
   SiVercel,
 } from "react-icons/si";
 import {
   TbBrandFramerMotion,
   TbBrandNextjs,
   TbBrandReactNative,
+  TbSql,
 } from "react-icons/tb";
 import {
   DiDjango,
   DiFirebase,
   DiGithub,
+  DiJava,
+  DiJavascript,
   DiMaterializecss,
   DiMongodb,
   DiMysql,
+  DiPhp,
   DiPostgresql,
+  DiPython,
+  DiRuby,
 } from "react-icons/di";
 import { BsBootstrap } from "react-icons/bs";
 import { FiFigma } from "react-icons/fi";
 import CategorizedTechs from "./components/CategorizedTechs";
+import { FaGolang } from "react-icons/fa6";
 
 export interface TechStacksInterface {
   icon: ReactNode;
@@ -41,21 +53,83 @@ export interface TechStacksInterface {
 export const categories: string[] = [
   "Frameworks",
   "Development Tools",
+  "Programming Languages",
   "Design Tool",
   "Tech Stacks",
   "Backend-As-A-Service",
-  "Database",
+  "Databases",
   "Libraries",
   "Microcontroller Platform",
+  "Object Relational Mapping",
 ];
 
 const iconStyle = "h-5 md:h-7 w-auto text-white dark:text-black";
 
 export const techStacks: TechStacksInterface[] = [
   {
-    icon: <FaReact className={iconStyle} />,
-    name: "React",
+    icon: <SiCplusplus className={iconStyle} />,
+    name: "C++",
+    category: "Programming Languages",
+  },
+  {
+    icon: <SiCsharp className={iconStyle} />,
+    name: "C#",
+    category: "Programming Languages",
+  },
+  {
+    icon: <DiJava className={iconStyle} />,
+    name: "Java",
+    category: "Programming Languages",
+  },
+  {
+    icon: <SiKotlin className={iconStyle} />,
+    name: "Kotlin",
+    category: "Programming Languages",
+  },
+  {
+    icon: <DiPython className={iconStyle} />,
+    name: "Python",
+    category: "Programming Languages",
+  },
+  {
+    icon: <DiPhp className={iconStyle} />,
+    name: "Php",
+    category: "Programming Languages",
+  },
+  {
+    icon: <SiTypescript className={iconStyle} />,
+    name: "Typescript",
+    category: "Programming Languages",
+  },
+  {
+    icon: <DiJavascript className={iconStyle} />,
+    name: "Javascript",
+    category: "Programming Languages",
+  },
+  {
+    icon: <DiRuby className={iconStyle} />,
+    name: "Ruby",
+    category: "Programming Languages",
+  },
+  {
+    icon: <FaGolang className={iconStyle} />,
+    name: "Go",
+    category: "Programming Languages",
+  },
+  {
+    icon: <TbSql className={iconStyle} />,
+    name: "SQL",
+    category: "Programming Languages",
+  },
+  {
+    icon: <SiJquery className={iconStyle} />,
+    name: "JQuery",
     category: "Libraries",
+  },
+  {
+    icon: <SiPrisma className={iconStyle} />,
+    name: "Prisma",
+    category: "Object Relational Mapping",
   },
   {
     icon: <TbBrandNextjs className={iconStyle} />,
@@ -115,17 +189,22 @@ export const techStacks: TechStacksInterface[] = [
   {
     icon: <DiMongodb className={iconStyle} />,
     name: "MongoDB",
-    category: "Database",
+    category: "Databases",
   },
   {
     icon: <DiPostgresql className={iconStyle} />,
     name: "Postgre SQL",
-    category: "Database",
+    category: "Databases",
+  },
+  {
+    icon: <SiSqlite className={iconStyle} />,
+    name: "SQLite",
+    category: "Databases",
   },
   {
     icon: <DiMysql className={iconStyle} />,
     name: "MySQL",
-    category: "Database",
+    category: "Databases",
   },
   {
     icon: <DiFirebase className={iconStyle} />,
