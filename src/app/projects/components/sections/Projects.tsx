0131b6d546db139d2b5454ea29f721fe8b5e744b
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-28 11:35:32 +0800

    added MyProjects component

--- src/app/projects/components/sections/Projects.tsx
+++ src/app/projects/components/sections/Projects.tsx
@@ -1,13 +1,12 @@
 import MotionTemplate from "@/app/components/motion/MotionTemplate";
 import React from "react";
-import ProjectsContainer from "../ProjectsContainer";
+import MyProjects from "./components/MyProjects";
 
 const Projects = () => {
   return (
     <MotionTemplate>
-      <div className="text-white dark:text-black text-center">
-        this is underdevelopment so view my projects on my github first hehe
-        <ProjectsContainer />
+      <div>
+        <MyProjects />
       </div>
     </MotionTemplate>
   );
--- src/app/projects/components/sections/components/ProjectsContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProjectsContainer = () => {
-  return <div className="grid grid-cols-1 md:grid-cols-2 gap-2"></div>;
-};
-
-export default ProjectsContainer;
